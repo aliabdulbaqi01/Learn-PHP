9700bc78885525e6c6a1bc2e409a37368bf31e14
aliabdulbaqi01 <aliabdulbaqi01@gmail.com>
2024-09-28 11:34:53 +0300

    todo app: validation, sanitization and middleware

--- mysql/workshop/src/bootstrap.php
+++ mysql/workshop/src/bootstrap.php
@@ -1,4 +1,9 @@
 <?php
 
 require_once __DIR__ . '/libs/helpers.php';
+require_once __DIR__ . '/libs/csrf.php';
+require_once __DIR__ . '/libs/flash.php';
 require_once __DIR__ . '/libs/auth.php';
+require_once __DIR__ . '/libs/view.php';
+require_once __DIR__ . '/libs/middleware.php';
+require_once __DIR__ . '/libs/filter.php';
---
+++ mysql/workshop/src/libs/middleware.php
@@ -0,0 +1,8 @@
+<?php
+
+function is_auth()
+{
+    if (!isset($_SESSION['auth'])) {
+
+    }
+}
